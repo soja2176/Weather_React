1a5e93168a78afc713fa5b34a3b35f0c41e81c51
soja2176 <san2176@hotmail.es>
2022-05-08 14:13:19 -0300

    Modularizando

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,9 @@ import sty from '../components/styles/About.module.css'
 export default function About(){
     return(
         <div className={sty.saludo}>
-            <h1>Hola bienvenido a mi app del clima!!!</h1>
+            <h1>Bienvenido a mi app del clima!!!</h1>
         <div>
-            <h2>Mi nombre es Santiago, soy estudiante de Henry en la carrera Full Stack Web Developer</h2>
+            <h2>Mi nombre es Santiago, soy estudiante de la carrera Full Stack Web Developer en Henry</h2>
         </div>
         </div>
     )
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,46 +1,74 @@
-// import Nav from '../nav.jsx';
-// import Cards from '../Cards.jsx';
-// import React, {useState} from 'react';
+import React, {useState} from 'react'
+import Nav from '../nav'
+import Cards from '../Cards.jsx'
+import About from '../About.jsx'
+import Ciudad from '../Ciudad.jsx'
+import {Route} from 'react-router-dom'
 
-// export default Home;
+export default Home;
 
-// function Home(){
-//     const [cities, setCities] = useState([]);
-//     function onSearch(ciudad) {
-//       const apiKey = '19c2c297eab5ce4b6ef3750b1b69a2a9'; 
-//       fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
-//         .then(r => r.json())
-//         .then((recurso) => {
-//           if(recurso.main !== undefined){
-//             const ciudad = {
-//               min: Math.round(recurso.main.temp_min),
-//               max: Math.round(recurso.main.temp_max),
-//               img: recurso.weather[0].icon,
-//               id: recurso.id,
-//               wind: recurso.wind.speed,
-//               temp: recurso.main.temp,
-//               name: recurso.name,
-//               weather: recurso.weather[0].main,
-//               clouds: recurso.clouds.all,
-//               latitud: recurso.coord.lat,
-//               longitud: recurso.coord.lon
-//             };
-//             setCities((oldCities) => {
-//               if (oldCities.some(c => c.name === ciudad.name)){
-//                 return oldCities;
-//               }else{
-//                 return [...oldCities, ciudad]
-//               }
-//             });
-//           } else {
-//             alert("Ciudad no encontrada");
-//           }
-//         });
+function Home(){
+    const [cities, setCities] = useState([]);
+    function onSearch(ciudad) {
+      const apiKey = '19c2c297eab5ce4b6ef3750b1b69a2a9'; 
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+        .then(r => r.json())
+        .then((recurso) => {
+          if(recurso.main !== undefined){
+            const ciudad = {
+              min: Math.round(recurso.main.temp_min),
+              max: Math.round(recurso.main.temp_max),
+              img: recurso.weather[0].icon,
+              id: recurso.id,
+              wind: recurso.wind.speed,
+              temp: recurso.main.temp,
+              name: recurso.name,
+              weather: recurso.weather[0].main,
+              clouds: recurso.clouds.all,
+              latitud: recurso.coord.lat,
+              longitud: recurso.coord.lon
+            };
+            setCities((oldCities) => {
+              if (oldCities.some(c => c.name === ciudad.name)){
+                return oldCities;
+              }else{
+                return [...oldCities, ciudad]
+              }
+            });
+          } else {
+            alert("Ciudad no encontrada");
+          }
+        });
   
-//       }
-//     function onClose(id) {
-//       setCities(oldCities => oldCities.filter(c => c.id !== id));
-//     }
-
-// }
+      }
+    //   function onFilter(ciudadId) {
+    //     let ciudad = cities.filter(c => c.id === parseInt(ciudadId));
+    //     if(ciudad.length > 0) {
+    //         return ciudad[0];
+    //     } else {
+    //         return null;
+    //     }
+    //   }
+    function onClose(id) {
+      setCities(oldCities => oldCities.filter(c => c.id !== id));
+    }
+   return( <div>
+    <Route path='/'>
+    <Nav onSearch={onSearch}/>
+    </Route>
+    <div>
+    <Route path='/' exact>
+    <Cards cities={cities} onClose={onClose}/>
+    </Route>
+    <Route path='/about' exact>
+    <About />
+    </Route>
+    <Route path="/ciudad/:ciudadId" exact render={({match}) => {
+      const city = cities.find(c=> c.id === Number(match.params.ciudadId))
+      return <Ciudad city={city} />
+    }}>
+    </Route>
+    </div>
+  </div>)
+}
 
